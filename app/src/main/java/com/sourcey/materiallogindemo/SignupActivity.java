f924ead43303cd760f6d3fa984265fe55acc525d
ISAAC\Isaac Kim <k0103373@gmail.com>
2017-05-18 01:51:22 +0900

    서버 연결 성공

--- app/src/main/java/com/sourcey/materiallogindemo/SignupActivity.java
+++ app/src/main/java/com/sourcey/materiallogindemo/SignupActivity.java
@@ -2,6 +2,7 @@ package com.sourcey.materiallogindemo;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -10,13 +11,31 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import butterknife.ButterKnife;
 import butterknife.Bind;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SignupActivity extends AppCompatActivity {
     private static final String TAG = "SignupActivity";
 
+    JSONParser jsonParser = new JSONParser();
+    // Progress Dialog
+    private ProgressDialog progressDialog;
+
+    // url to create new product
+    private static String url_create_product = "http://10.0.2.2/db/create.php";
+
+    // JSON Node names
+    private static final String TAG_SUCCESS = "success";
 
     @Bind(R.id.input_email) EditText _emailText;
     @Bind(R.id.input_password) EditText _passwordText;
@@ -35,7 +54,8 @@ public class SignupActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Finish the registration screen and return to the Login activity
-                signup();
+                new CreateNewProduct().execute();
+                //signup();
             }
         });
     }
@@ -49,40 +69,56 @@ public class SignupActivity extends AppCompatActivity {
         super.onBackPressed();
     }
 
+    class CreateNewProduct extends AsyncTask<String, String, String> {
 
-    public void signup() {
-        Log.d(TAG, "Signup");
-
-        if (!validate()) {
-            onSignupFailed();
-            return;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(SignupActivity.this, R.style.Progress_Dialog);
+            progressDialog.setIndeterminate(true);
+            progressDialog.setMessage("Creating Account...");
+            progressDialog.show();
         }
 
-        _signupButton.setEnabled(false);
+        protected String doInBackground(String... args) {
 
-        final ProgressDialog progressDialog = new ProgressDialog(SignupActivity.this, R.style.Progress_Dialog);
-        progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Creating Account...");
-        progressDialog.show();
+            String email = _emailText.getText().toString();
+            String passwd = _passwordText.getText().toString();
+            String reEnterPassword = _reEnterPasswordText.getText().toString();
+            String name = _nameText.getText().toString();
 
-        String name = _nameText.getText().toString();
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
-        String reEnterPassword = _reEnterPasswordText.getText().toString();
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("email", email));
+            params.add(new BasicNameValuePair("passwd", passwd));
+            params.add(new BasicNameValuePair("name", name));
 
-        // TODO: Implement your own signup logic here.
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        // On complete call either onSignupSuccess or onSignupFailed
-                        // depending on success
-                        onSignupSuccess();
-                        // onSignupFailed();
-                        progressDialog.dismiss();
-                    }
-                }, 3000);
+            // getting JSON Object
+            // Note that create product url accepts POST method
+            JSONObject json = jsonParser.makeHttpRequest(url_create_product, "POST", params);
 
-        overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+            // check log cat fro response
+            Log.d("Create Response", json.toString());
+
+            // check for success tag
+            try {
+                int success = json.getInt(TAG_SUCCESS);
+
+                if (success == 1) {
+                    // successfully created product
+                    onSignupSuccess();
+                } else {
+                    // failed to create product
+                    onSignupFailed();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog once done
+            progressDialog.dismiss();
+        }
     }
 
     public void onSignupSuccess() {
@@ -95,7 +131,6 @@ public class SignupActivity extends AppCompatActivity {
 
     public void onSignupFailed() {
         Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
-
         _signupButton.setEnabled(true);
     }
 
