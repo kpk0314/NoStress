67d0a515356a2932d1810d8d02a9d58e00682635
p.k.KIM <kpk0314@naver.com>
2017-05-22 10:12:59 +0900

    개인정보 입력

--- app/src/main/java/com/sourcey/materiallogindemo/PrivacyActivity.java
+++ app/src/main/java/com/sourcey/materiallogindemo/PrivacyActivity.java
@@ -1,23 +1,40 @@
 package com.sourcey.materiallogindemo;
 
+import android.app.ActivityManager;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import butterknife.ButterKnife;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class PrivacyActivity extends AppCompatActivity {
     private static final String TAG = "PrivacyActivity";
 
+    JSONParser jsonParser = new JSONParser();
+
+    private ProgressDialog progressDialog;
+
+    // url to create new product
+    private static String url_create_product = "http://10.0.2.2/db/create_private.php";
+
+    // JSON Node names
+    private static final String TAG_SUCCESS = "success";
 
     @Bind(R.id.input_birth) EditText _birthText;
     @Bind(R.id.input_gender) EditText _genderText;
@@ -39,72 +56,125 @@ public class PrivacyActivity extends AppCompatActivity {
             }
         });
     }
+    /* @Override
+     public void onBackPressed() {
+         overridePendingTransition(0, 0);
+         Intent intent = new Intent(this, LoginActivity.class);
+         startActivity(intent);
+         finish();
+         super.onBackPressed();
+     }
 
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        switch (keyCode) {
-            case KeyEvent.KEYCODE_BACK:
-                return true;
-        }
-        return super.onKeyDown(keyCode, event);
-    }
+     @Override
+     public boolean onKeyDown(int keyCode, KeyEvent event) {
+         switch (keyCode) {
+             case KeyEvent.KEYCODE_BACK:
+                 return true;
+         }
+         return super.onKeyDown(keyCode, event);
+     }
+ */
+
+
 
     public void signup() {
         Log.d(TAG, "Signup");
 
-        if (!validate()) {
+       /* if (!validate()) {
             onSignupFailed();
             return;
-        }
-
-        _signupButton.setEnabled(false);
-
-        final ProgressDialog progressDialog = new ProgressDialog(PrivacyActivity.this, R.style.Progress_Dialog);
-        progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Creating Account...");
-        progressDialog.show();
-
-        String name = _birthText.getText().toString();
-        String email = _genderText.getText().toString();
-        String password = _heightText.getText().toString();
-        String reEnterPassword = _weightText.getText().toString();
-
-        // TODO: Implement your own signup logic here.
-
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        // On complete call either onSignupSuccess or onSignupFailed
-                        // depending on success
-                        onSignupSuccess();
-                        // onSignupFailed();
-                        progressDialog.dismiss();
-                    }
-                }, 3000);
+        }*/
+        new CreateNewProduct().execute();
     }
 
+    class CreateNewProduct extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+
+            progressDialog = new ProgressDialog(PrivacyActivity.this, R.style.Progress_Dialog);
+            progressDialog.setIndeterminate(true);
+            progressDialog.setMessage("Creating Account...");
+            progressDialog.show();
+
+        }
+
+        protected String doInBackground(String... args) {
+
+            String birth = _birthText.getText().toString();
+            String gender = _genderText.getText().toString();
+            String height = _heightText.getText().toString();
+            String weight = _weightText.getText().toString();
+
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("birth", birth));
+            params.add(new BasicNameValuePair("gender", gender));
+            params.add(new BasicNameValuePair("height", height));
+            params.add(new BasicNameValuePair("weight", weight));
+
+            // getting JSON Object
+            // Note that create product url accepts POST method
+            JSONObject json = jsonParser.makeHttpRequest(url_create_product, "POST", params);
+
+
+            // check log cat fro response
+            Log.d("Create Response", json.toString());
+
+            // check for success tag
+            try {
+                int success = json.getInt(TAG_SUCCESS);
+
+                if (success == 1) {
+                    // successfully created product
+                    onSignupSuccess();
+                } else {
+                    // failed to create product
+                    onSignupFailed();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog once done
+            progressDialog.dismiss();
+        }
+
+
+
+
+    }
 
     public void onSignupSuccess() {
         _signupButton.setEnabled(true);
         setResult(RESULT_OK, null);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
         finish();
     }
 
     public void onSignupFailed() {
-        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
-
+        Toast.makeText(getBaseContext(), "private failed", Toast.LENGTH_LONG).show();
         _signupButton.setEnabled(true);
     }
 
     public boolean validate() {
         boolean valid = true;
 
-        String name = _birthText.getText().toString();
-        String email = _genderText.getText().toString();
-        String password = _heightText.getText().toString();
-        String reEnterPassword = _weightText.getText().toString();
+        String birth = _birthText.getText().toString();
+        String gender = _genderText.getText().toString();
+        String height = _heightText.getText().toString();
+        String weight = _weightText.getText().toString();
 
 
         return valid;
     }
+
+
+
 }
+
--- app/src/main/java/com/sourcey/materiallogindemo/SignupActivity.java
+++ app/src/main/java/com/sourcey/materiallogindemo/SignupActivity.java
@@ -132,7 +132,7 @@ public class SignupActivity extends AppCompatActivity {
         protected void onPostExecute(String file_url) {
             // dismiss the dialog once done
             progressDialog.dismiss();
-        }
+       }
     }
 
     public void onSignupSuccess() {
