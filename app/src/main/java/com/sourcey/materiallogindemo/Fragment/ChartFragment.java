dd974f189d6e0f2727442ddc232bdd95e5b05a3f
ISAAC\Isaac Kim <k0103373@gmail.com>
2017-06-04 21:56:50 +0900

    차트 액티비티에 Candle Stick Chart 넣는 중

--- app/src/main/java/com/sourcey/materiallogindemo/Fragment/ChartFragment.java
+++ app/src/main/java/com/sourcey/materiallogindemo/Fragment/ChartFragment.java
@@ -1,12 +1,29 @@
 package com.sourcey.materiallogindemo.Fragment;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.github.mikephil.charting.charts.CandleStickChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.CandleData;
+import com.github.mikephil.charting.data.CandleDataSet;
+import com.github.mikephil.charting.data.CandleEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.sourcey.materiallogindemo.R;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import lecho.lib.hellocharts.model.AxisValue;
+import lecho.lib.hellocharts.model.PointValue;
 
 
 /**
@@ -24,7 +41,68 @@ public class ChartFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chart, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_chart, container, false);
+
+        CandleStickChart candleStickChart = (CandleStickChart) rootView.findViewById(R.id.chart);
+
+        ArrayList<CandleEntry> entries = new ArrayList<>();
+        entries.add(new CandleEntry(0, 46.2f, 20.2f, 27.0f, 41.3f));
+        entries.add(new CandleEntry(1, 55.0f, 27.0f, 33.5f, 49.6f));
+        entries.add(new CandleEntry(2, 52.5f, 30.2f, 35.0f, 45.0f));
+        entries.add(new CandleEntry(3, 60.0f, 32.5f, 44.0f, 50.0f));
+        entries.add(new CandleEntry(4, 56.7f, 20.0f, 28.0f, 45.0f));
+        entries.add(new CandleEntry(5, 46.2f, 20.2f, 27.0f, 41.3f));
+        entries.add(new CandleEntry(6, 55.0f, 27.0f, 33.5f, 49.6f));
+
+        CandleDataSet dataset = new CandleDataSet(entries, "# of Calls");
+
+        ArrayList<String> labels = new ArrayList<String>();
+
+        for (int i = 1; i <= 7; i++) {
+            Date currentDate = new Date(System.currentTimeMillis() - TimeUnit.DAYS.toMillis(7 - i));
+            SimpleDateFormat sdf = new SimpleDateFormat("E");
+            String day = sdf.format(currentDate);
+            labels.add(day);
+        }
+
+        CandleData data = new CandleData(labels, dataset);
+        candleStickChart.setData(data);
+        int[] black = {Color.BLACK, Color.BLACK};
+        dataset.setColors(black);
+        dataset.setValueTextSize(15);
+
+        // dataset.setColors(ColorTemplate.COLORFUL_COLORS); //
+        candleStickChart.setBackgroundColor(Color.TRANSPARENT); //set whatever color you prefer
+        candleStickChart.setDrawGridBackground(false);// this is a must
+
+        XAxis xAxis = candleStickChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        xAxis.setDrawAxisLine(true);
+        xAxis.setAxisLineColor(Color.BLACK);
+        xAxis.setTextSize(15);
+
+
+        candleStickChart.getLegend().setEnabled(false);
+        candleStickChart.setDescription("");
+        candleStickChart.getAxisLeft().setDrawLabels(false);
+        candleStickChart.getAxisRight().setDrawLabels(false);
+        candleStickChart.getAxisLeft().setDrawGridLines(false);
+        candleStickChart.getAxisLeft().setDrawAxisLine(false);
+        candleStickChart.getAxisLeft().setAxisMinValue(80f);
+        candleStickChart.getAxisRight().setAxisMinValue(80f);
+        candleStickChart.getAxisRight().setDrawGridLines(false);
+        candleStickChart.getAxisRight().setDrawAxisLine(false);
+        candleStickChart.setPinchZoom(false);
+        candleStickChart.setDoubleTapToZoomEnabled(false);
+        candleStickChart.setTouchEnabled(false);
+        candleStickChart.setDescriptionTextSize(15);
+
+        // candleStickChart.setAutoScaleMinMaxEnabled(true);
+        candleStickChart.invalidate();
+
+        return rootView;
     }
 
 }
--- app/src/main/java/com/sourcey/materiallogindemo/Fragment/MainFragment.java
+++ app/src/main/java/com/sourcey/materiallogindemo/Fragment/MainFragment.java
@@ -134,94 +134,10 @@ public class MainFragment extends Fragment {
 
         int test[] = {30, 20, 15, 18, 60, 30, 10, 50, 60, 32, 24, 45};
 
-//        mChart = (LineChart) rootView.findViewById(R.id.chart1);
-//        mChart.setDrawGridBackground(false);
-//        // no description text
-//        mChart.getDescription().setEnabled(false);
-//        // enable touch gestures
-//        mChart.setTouchEnabled(true);
-//        // enable scaling and dragging
-//        mChart.setDragEnabled(true);
-//        mChart.setScaleEnabled(true);
-//        // mChart.setScaleXEnabled(true);
-//        // mChart.setScaleYEnabled(true);
-//        // if disabled, scaling can be done on x- and y-axis separately
-//        mChart.setPinchZoom(true);
-//
-//        int[] dataObjects = {30, 20, 15, 18, 60, 30, 10, 50, 60, 32, 24, 45};
-//
-//        List<Entry> entries = new ArrayList<Entry>();
-//
-//        for(int i = 0; i < 12; i ++) {
-//            entries.add(new Entry(dataObjects[i], i));
-//        }
-//
-//        LineDataSet setData = new LineDataSet(entries, "TEST 1");
-//        setData.setAxisDependency(YAxis.AxisDependency.LEFT);
-//
-//        ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
-//        dataSets.add(setData);
-//
-//        ArrayList<String> xVals = new ArrayList<String>();
-//        xVals.add("PM 5");
-//        xVals.add("PM 8");
-//        xVals.add("PM 11");
-//        xVals.add("AM 2");
-//        xVals.add("AM 5");
-//        xVals.add("AM 8");
-//        xVals.add("AM 11");
-//        xVals.add("PM 2");
-//        xVals.add("PM 5");
-//
-//        LineData data = new LineData(dataSets);
-//        mChart.setData(data);
-//
-
-//        List<PointValue> values = new ArrayList<PointValue>();
-//        values.add(new PointValue(0, 2));
-//        values.add(new PointValue(1, 4));
-//        values.add(new PointValue(2, 3));
-//        values.add(new PointValue(3, 4));
-//        values.add(new PointValue(4, 2));
-//        values.add(new PointValue(5, 4));
-//        values.add(new PointValue(6, 3));
-//        values.add(new PointValue(7, 4));
-//        values.add(new PointValue(8, 4));
-//
-//
-//        //In most cased you can call data model methods in builder-pattern-like manner.
-//        Line line = new Line(values).setColor(Color.WHITE).setCubic(true);
-//        List<Line> lines = new ArrayList<Line>();
-//        lines.add(line);
-//
-//        LineChartData data = new LineChartData();
-//        data.setLines(lines);
-//
-//        LineChartView chart = (LineChartView) rootView.findViewById(R.id.chart);
-//        chart.setLineChartData(data);
-//        chart.setViewportCalculationEnabled(false);
         chart = (LineChartView) rootView.findViewById(R.id.chart);
 
         generateValues();
         generateData();
-
-
-//        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{
-//                new DataPoint(0, test[0]),
-//                new DataPoint(1, test[1]),
-//                new DataPoint(2, test[2]),
-//                new DataPoint(3, test[3]),
-//                new DataPoint(4, test[4]),
-//                new DataPoint(5, test[5]),
-//                new DataPoint(6, test[6]),
-//                new DataPoint(7, test[7]),
-//                new DataPoint(8, test[8]),
-//                new DataPoint(9, test[9]),
-//                new DataPoint(10, test[10]),
-//                new DataPoint(11, test[11]),
-//        });
-//        graph.addSeries(series);
-
 
         // 화면 아래로 당겨서 업데이트
         mSwipeRefresh = (SwipeRefreshLayout) rootView.findViewById(R.id.swiperefresh);
