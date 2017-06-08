bd07beaee813b4b7c9b92f885abd9bf48ff5efca
ISAAC\Isaac Kim <k0103373@gmail.com>
2017-06-08 19:44:53 +0900

    리스트뷰 구현..휴

---
+++ app/src/main/java/com/sourcey/materiallogindemo/BinderData.java
@@ -0,0 +1,107 @@
+package com.sourcey.materiallogindemo;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.sourcey.materiallogindemo.Fragment.ChartFragment;
+
+public class BinderData extends BaseAdapter {
+
+    // XML node keys
+    static final String KEY_TAG = "listdata"; // parent node
+    static final String KEY_ID = "id";
+    static final String KEY_DAY_OF_WEEK = "day_of_week";
+    static final String KEY_DATE = "date";
+    static final String KEY_AVERAGE = "average";
+    static final String KEY_MAXIMUM = "maximum";
+    static final String KEY_MINIMUM = "minimum";
+
+    LayoutInflater inflater;
+    List<HashMap<String,String>> dataCollection;
+    ViewHolder holder;
+    public BinderData() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public BinderData(ChartFragment act, List<HashMap<String,String>> map) {
+
+        this.dataCollection = map;
+
+        inflater = (LayoutInflater) act.getActivity()
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+
+    public int getCount() {
+        // TODO Auto-generated method stub
+//		return idlist.size();
+        return dataCollection.size();
+    }
+
+    public Object getItem(int arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View vi=convertView;
+        if(convertView==null){
+
+            vi = inflater.inflate(R.layout.list_row, null);
+            holder = new ViewHolder();
+
+            holder.day_of_week = (TextView)vi.findViewById(R.id.day_of_week); // city name
+            holder.date = (TextView)vi.findViewById(R.id.date); // city weather overview
+            holder.average =  (TextView)vi.findViewById(R.id.average); // city temperature
+            holder.maximum =(TextView)vi.findViewById(R.id.maximum); // thumb image
+            holder.minimum =(TextView)vi.findViewById(R.id.minimum); // thumb image
+
+            vi.setTag(holder);
+        }
+        else{
+
+            holder = (ViewHolder)vi.getTag();
+        }
+
+        // Setting all values in listview
+        holder.day_of_week.setText(dataCollection.get(position).get(KEY_DAY_OF_WEEK));
+        holder.date.setText(dataCollection.get(position).get(KEY_DATE));
+        holder.average.setText(dataCollection.get(position).get(KEY_AVERAGE));
+        holder.maximum.setText(dataCollection.get(position).get(KEY_MAXIMUM));
+        holder.minimum.setText(dataCollection.get(position).get(KEY_MINIMUM));
+
+        return vi;
+    }
+
+    /*
+     *
+     * */
+    static class ViewHolder{
+
+        TextView day_of_week;
+        TextView date;
+        TextView average;
+        TextView maximum;
+        TextView minimum;
+    }
+
+}
--- app/src/main/java/com/sourcey/materiallogindemo/Fragment/ChartFragment.java
+++ app/src/main/java/com/sourcey/materiallogindemo/Fragment/ChartFragment.java
@@ -4,29 +4,28 @@ package com.sourcey.materiallogindemo.Fragment;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.RadioButton;
 
-import com.github.mikephil.charting.charts.CandleStickChart;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.data.CandleData;
-import com.github.mikephil.charting.data.CandleDataSet;
-import com.github.mikephil.charting.data.CandleEntry;
-import com.github.mikephil.charting.utils.ColorTemplate;
+import com.sourcey.materiallogindemo.BinderData;
 import com.sourcey.materiallogindemo.R;
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
-import lecho.lib.hellocharts.model.AxisValue;
-import lecho.lib.hellocharts.model.PointValue;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 
 /**
@@ -35,9 +34,22 @@ import lecho.lib.hellocharts.model.PointValue;
 public class ChartFragment extends Fragment {
 
 
+    // XML node keys
+    static final String KEY_TAG = "listdata"; // parent node
+    static final String KEY_ID = "id";
+    static final String KEY_DAY_OF_WEEK = "day_of_week";
+    static final String KEY_DATE = "date";
+    static final String KEY_AVERAGE = "average";
+    static final String KEY_MAXIMUM = "maximum";
+    static final String KEY_MINIMUM = "minimum";
 
     RadioButton week_btn;
     RadioButton month_btn;
+    View rootView;
+    ListView listView;
+    BinderData adapter = null;
+    List<HashMap<String, String>> dataCollection;
+    boolean selected = true;
 
     public ChartFragment() {
         // Required empty public constructor
@@ -49,49 +61,142 @@ public class ChartFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        View rootView = inflater.inflate(R.layout.fragment_chart, container, false);
-
+        rootView = inflater.inflate(R.layout.fragment_chart, container, false);
         week_btn = (RadioButton) rootView.findViewById(R.id.week_btn);
         month_btn = (RadioButton) rootView.findViewById(R.id.month_btn);
 
-        // ArrayAdapter를 통해 LIST로 표시할 오브젝트를 지정한다.
-        // 여기서는 심플하게 그냥 String
-        // 레이아웃 android.R.layout.simple_list_item_1 는 안드로이드가 기본적으로 제공하는 간단한 아이템 레이아웃
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1);
+        refreshListView();
 
-        // 아이템을 추가
-        adapter.add("item1");
-        adapter.add("item2");
-        adapter.add("item3");
 
-        // ListView 가져오기
-        ListView listView = (ListView) rootView.findViewById(R.id.listView);
-
-        // ListView에 각각의 아이템표시를 제어하는 Adapter를 설정
-        listView.setAdapter(adapter);
-
+//        // ArrayAdapter를 통해 LIST로 표시할 오브젝트를 지정한다.
+//        // 여기서는 심플하게 그냥 String
+//        // 레이아웃 android.R.layout.simple_list_item_1 는 안드로이드가 기본적으로 제공하는 간단한 아이템 레이아웃
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1);
+//        // 아이템을 추가
+//        adapter.add("화요일");
+//        adapter.add("item2");
+//        adapter.add("item3");
+//        // ListView 가져오기
+//        listView = (ListView) rootView.findViewById(R.id.listView);
+//        // ListView에 각각의 아이템표시를 제어하는 Adapter를 설정
+//        listView.setAdapter(adapter);
 
 
         week_btn.setOnClickListener(optionOnClickListener);
         month_btn.setOnClickListener(optionOnClickListener);
         week_btn.setChecked(true);
 
-
-
         return rootView;
+
+    }
+
+
+    void refreshListView(){
+        try {
+            DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+            Document week_doc = docBuilder.parse(getActivity().getAssets().open("weekdata.xml"));
+            Document month_doc = docBuilder.parse(getActivity().getAssets().open("monthdata.xml"));
+
+            dataCollection = new ArrayList<HashMap<String, String>>();
+
+            // normalize text representation
+            week_doc.getDocumentElement().normalize();
+            month_doc.getDocumentElement().normalize();
+
+            NodeList dataList;
+            if(selected){
+                dataList = week_doc.getElementsByTagName("listdata");
+            } else {
+                dataList = month_doc.getElementsByTagName("listdata");
+            }
+
+
+            HashMap<String, String> map = null;
+
+            for (int i = 0; i < dataList.getLength(); i++) {
+
+                map = new HashMap<String, String>();
+
+                Node firstDataNode = dataList.item(i);
+
+                if (firstDataNode.getNodeType() == Node.ELEMENT_NODE) {
+
+                    Element firstDataElement = (Element) firstDataNode;
+                    //-------
+                    NodeList idList = firstDataElement.getElementsByTagName(KEY_ID);
+                    Element firstIdElement = (Element) idList.item(0);
+                    NodeList textIdList = firstIdElement.getChildNodes();
+                    //--id
+                    map.put(KEY_ID, ((Node) textIdList.item(0)).getNodeValue().trim());
+
+                    //2.-------
+                    NodeList weekList = firstDataElement.getElementsByTagName(KEY_DAY_OF_WEEK);
+                    Element firstWeekElement = (Element) weekList.item(0);
+                    NodeList textWeekList = firstWeekElement.getChildNodes();
+                    //--city
+                    map.put(KEY_DAY_OF_WEEK, ((Node) textWeekList.item(0)).getNodeValue().trim());
+
+                    //3.-------
+                    NodeList dateList = firstDataElement.getElementsByTagName(KEY_DATE);
+                    Element firstDateElement = (Element) dateList.item(0);
+                    NodeList textDateList = firstDateElement.getChildNodes();
+                    //--city
+                    map.put(KEY_DATE, ((Node) textDateList.item(0)).getNodeValue().trim());
+
+                    //4.-------
+                    NodeList averageList = firstDataElement.getElementsByTagName(KEY_AVERAGE);
+                    Element firstAverageElement = (Element) averageList.item(0);
+                    NodeList textAverageList = firstAverageElement.getChildNodes();
+                    //--city
+                    map.put(KEY_AVERAGE, ((Node) textAverageList.item(0)).getNodeValue().trim());
+
+                    //5.-------
+                    NodeList maximumList = firstDataElement.getElementsByTagName(KEY_MAXIMUM);
+                    Element firstMaximumElement = (Element) maximumList.item(0);
+                    NodeList textMaximumList = firstMaximumElement.getChildNodes();
+                    //--city
+                    map.put(KEY_MAXIMUM, ((Node) textMaximumList.item(0)).getNodeValue().trim());
+
+                    //6.-------
+                    NodeList minimumList = firstDataElement.getElementsByTagName(KEY_MINIMUM);
+                    Element firstMinimumElement = (Element) minimumList.item(0);
+                    NodeList textMinimumList = firstMinimumElement.getChildNodes();
+                    //--city
+                    map.put(KEY_MINIMUM, ((Node) textMinimumList.item(0)).getNodeValue().trim());
+
+                    //Add to the Arraylist
+                    dataCollection.add(map);
+                }
+            }
+
+            BinderData bindingData = new BinderData(this, dataCollection);
+            listView = (ListView) rootView.findViewById(R.id.listView);
+            Log.i("BEFORE", "<<------------- Before SetAdapter-------------->>");
+            listView.setAdapter(bindingData);
+            Log.i("AFTER", "<<------------- After SetAdapter-------------->>");
+
+        } catch (IOException ex) {
+            Log.e("Error", ex.getMessage());
+        } catch (Exception ex) {
+            Log.e("Error", "Loading exception");
+        }
     }
 
     RadioButton.OnClickListener optionOnClickListener
             = new RadioButton.OnClickListener() {
         public void onClick(View v) {
-            if(week_btn.isChecked()){
+            if (week_btn.isChecked()) {
                 week_btn.setTextColor(Color.BLACK);
                 month_btn.setTextColor(Color.WHITE);
+                selected = true;
+
             } else {
                 week_btn.setTextColor(Color.WHITE);
                 month_btn.setTextColor(Color.BLACK);
+                selected = false;
             }
-
+            refreshListView();
         }
     };
 
