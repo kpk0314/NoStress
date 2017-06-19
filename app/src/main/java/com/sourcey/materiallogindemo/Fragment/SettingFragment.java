1ff57f7cbeb303595c5ab41854f9c093ac9c6fdf
ISAAC\Isaac Kim <k0103373@gmail.com>
2017-06-19 22:36:09 +0900

    setting fragment 리스트뷰 구현중

--- app/src/main/java/com/sourcey/materiallogindemo/Fragment/SettingFragment.java
+++ app/src/main/java/com/sourcey/materiallogindemo/Fragment/SettingFragment.java
@@ -6,6 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.sourcey.materiallogindemo.R;
 
@@ -16,10 +20,11 @@ import com.sourcey.materiallogindemo.R;
 public class SettingFragment extends Fragment {
 
     View rootView;
-
+    ListView listView ;
 
     public SettingFragment() {
         // Required empty public constructor
+
     }
 
 
@@ -28,6 +33,45 @@ public class SettingFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         rootView = inflater.inflate(R.layout.fragment_setting, container, false);
+        listView = (ListView) rootView.findViewById(R.id.setting_list);
+
+        // Defined Array values to show in ListView
+        String[] values = new String[] { "Android List View",
+                "Adapter implementation",
+                "Simple List View In Android",
+                "Create List View Android",
+                "Android Example",
+                "List View Source Code",
+                "List View Array Adapter",
+                "Android Example List View"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(rootView.getContext(),  android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
+        // ListView Item Click Listener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // ListView Clicked item index
+                int itemPosition     = position;
+
+                // ListView Clicked item value
+                String  itemValue    = (String) listView.getItemAtPosition(position);
+
+                // Show Alert
+                Toast.makeText(rootView.getContext(),
+                        "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
+                        .show();
+            }
+
+        });
+
 
 
         return rootView;
